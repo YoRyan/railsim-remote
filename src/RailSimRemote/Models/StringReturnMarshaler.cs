9f5526061335520f625c9224f97778b19394579c
Ryan Young <ryan@youngryan.com>
2020-02-28 11:18:00 -0800

    style

--- src/RailSimRemote/Models/StringReturnMarshaler.cs
+++ src/RailSimRemote/Models/StringReturnMarshaler.cs
@@ -6,24 +6,16 @@ namespace RailSimRemote.Models
     public class StringReturnMarshaler : ICustomMarshaler
     {
         private static StringReturnMarshaler static_instance;
-        public void CleanUpManagedData(object ManagedObj)
-        {
-        }
-
-        public void CleanUpNativeData(IntPtr pNativeData)
-        {
-        }
-
+        public void CleanUpManagedData(object ManagedObj) {}
+        public void CleanUpNativeData(IntPtr pNativeData) {}
         public int GetNativeDataSize()
         {
             return -1;
         }
-
         public IntPtr MarshalManagedToNative(object ManagedObj)
         {
             return (IntPtr)0;
         }
-
         public object MarshalNativeToManaged(IntPtr pNativeData)
         {
             return Marshal.PtrToStringAnsi(pNativeData);

f402c3e63137e315b09c30e9534891d18c509cd9
Ryan Young <ryan@youngryan.com>
2020-02-28 18:20:49 -0800

    Delete unused route config.

--- src/RailSimRemote/App_Start/RouteConfig.cs
+++ src/RailSimRemote/App_Start/RouteConfig.cs
@@ -9,15 +9,6 @@ namespace RailSimRemote
 {
     public class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
+        public static void RegisterRoutes(RouteCollection routes) {}
     }
 }

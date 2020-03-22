77bdf250f92f64559b4856deafece729aa81b406
Ryan Young <ryan@youngryan.com>
2020-03-21 23:02:09 -0700

    Add a CLI argument to change the listening address.

--- src/RailSimRemote/Program.cs
+++ src/RailSimRemote/Program.cs
@@ -5,11 +5,13 @@ namespace RailSimRemote
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <param name="urls">The URL(s) the web server will listen on.</param>
+        static void Main(string urls = "http://localhost:8888")
         {
             var host = WebHost.CreateDefaultBuilder()
                 .UseKestrel()
                 .UseStartup<Startup>()
+                .UseUrls(urls)
                 .Build();
             host.Run();
         }
